8f625b62e3cf5708e3935561a18e81423e87a941
Ashwani Pandey <ashwani.pandey@rudrainnovative.in>
2025-09-08 16:44:36 +0530

    Created pages and routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
+import React from 'react'
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
+import Home from "./pages/Home.jsx"
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+import Profile from './pages/Profile.jsx'
+import About from './pages/About.jsx'
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
