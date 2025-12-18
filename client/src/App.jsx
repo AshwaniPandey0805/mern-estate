41273c5ced51dbca9d7c38a69527d48591ae5e90
ashwani <ashwanipandey679@gmail.com>
2025-12-18 19:43:54 +0530

    Added header component.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
-// import './App.css'
+import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from './pages/Home.jsx'
 import SignIn from './pages/SignIn.jsx'
 import SignUp from './pages/SignUp.jsx'
 import About from './pages/About.jsx'
 import Profile from './pages/Profile.jsx' 
+import Header from "./components/Header.jsx"
 function App() {
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
