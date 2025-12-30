67e1d4e185f7694ee96f09fc7c423beee3820f92
ashwani <ashwanipandey679@gmail.com>
2025-12-30 18:25:59 +0530

    added home and about page.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,8 +12,8 @@ function Header() {
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
                 <Link to='/' >
                     <h1 className='font-bold text-sm sm:text-xl flex flex-wrap hover:cursor-pointer'>
-                        <span className='text-slate-500' >Sahand</span>
-                        <span className='text-slate-700' >Estate</span>
+                        <span className='text-slate-500' >MERN</span>
+                        <span className='text-slate-700' >Auth</span>
                     </h1>
                 </Link>
                 <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
@@ -25,7 +25,7 @@ function Header() {
                     <FaSearch className='text-slate-600 hover:cursor-pointer' />
                 </form>
                 <ul className='flex gap-4'>
-                    <Link to='/home' >
+                    <Link to='/' >
                         <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Home</li>
                     </Link>
                     <Link to='/about' >
