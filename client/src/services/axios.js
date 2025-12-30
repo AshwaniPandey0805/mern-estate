1db71bc7d6747eb943f0141d00909e5a12c99708
ashwani <ashwanipandey679@gmail.com>
2025-12-30 19:56:04 +0530

    addde live url to make request.

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : "http://localhost:3000/api", // base url
+    baseURL : "https://mern-estate-2-fv0l.onrender.com/api", // base url
     timeout : 10000,
     headers : {
         "Content-Type" : "application/json",
